fc6b46ca4e0357abe7134f619505bfe49d571b6f
Mark <markshappert@gmail.com>
2025-12-30 17:52:01 +0100

    Added game icon

--- src/main/java/com/blackjack/view/BlackjackApp.java
+++ src/main/java/com/blackjack/view/BlackjackApp.java
@@ -4,8 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class BlackjackApp extends Application {
 
@@ -18,6 +21,9 @@ public class BlackjackApp extends Application {
         Scene scene = new Scene(loader.load(), 800, 600);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setTitle("BlackJack 21");
+        stage.getIcons().add(
+                new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/blackjack/view/icon.png")))
+        );
         stage.setScene(scene);
         stage.show();
     }
